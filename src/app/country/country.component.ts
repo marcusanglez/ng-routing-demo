21eaca90309bf522abfbbde37fb05d1056661444
Marco <emarcus.g@gmail.com>
2023-09-20 15:16:51 -0700

    first commit

---
+++ src/app/country/country.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country',
+  templateUrl: `
+    <p>
+      country works!
+    </p>
+  `,
+  styleUrls: ['./country.component.css']
+})
+export class CountryComponent {
+
+}
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  template: `
+    <p>
+      person works!
+    </p>
+  `,
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent {
+
+}
